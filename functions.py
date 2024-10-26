523aa57c65bf8c8cb71a1b1ad21ce5f6e50ebc01
DavidMark254 <davinjiski@gmail.com>
2024-10-26 18:25:41 +0300

    first commit

---
+++ functions.py
@@ -0,0 +1,10 @@
+def my_function():
+    print("Good Morning Andrew")
+    print("Good Morning Peter")
+    print("Good Morning Ann")
+
+    my_function()
+    my_function()
+    my_function()
+
+#write a function to sum all the numbers in a list
